17930f91556feb938c8dc7aefc61563880a1a1fc
K R I S H N A <krishnakripa34567@gmail.com>
2024-05-31 00:52:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/Krishna-s-Hub/device_xiaomi_sweet device/xiaomi/sweet
-
-#custom repos
-rm -rf prebuilts/clang/host/linux-x86/clang-r498229b
-git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/clang-r498229b
-
-git clone https://github.com/pure-soul-kk/hardware_xiaomi 
+rm -rf device/xiaomi
+git clone https://github.com/Krishna-s-Hub/device_xiaomi_sweet device/xiaomi/sweet 
 
 #build
 . build/envsetup.sh
